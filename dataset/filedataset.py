45049cdfbdf195a55ab0bf29a71be1aba58dac96
Kshitij Sharma <kshitij4sharma@gmail.com>
2025-01-28 16:28:30 +0000

    Update

---
+++ dataset/filedataset.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import os
+from tqdm import tqdm
+from PIL import Image as image
+
+from .basedataset import BaseDataset
+
+class FileDataset(BaseDataset):
+    def __init__(self, dataset_file, dataset_folder, transform=None):
+        self.dataset_folder = dataset_folder
+        self.transform = transform
+        # self.images, self.coordinates = self.load_dataset(dataset_file)
+
+    def load_dataset(self, dataset_file):
+        try:
+            dataset_info = pd.read_csv(dataset_file)
+        except Exception as e:
+            raise IOError(f"Error reading {dataset_file}: {e}")
+
+        images = []
+        coordinates = []
+
+        for _, row in tqdm(dataset_info.iterrows(), desc="Loading image paths and coordinates"):
+            filename = os.path.join(self.dataset_folder, row[self.image_column_name])
+            if os.path.exists(filename):
+                images.append(filename)
+                latitude = float(row['lat'])
+                longitude = float(row['lon'])
+                coordinates.append((latitude, longitude))
+
+        self.images, self.coordinates = images, coordinates
+    
+    def __len__(self):
+        return len(self.images)
+    
+    def set_columns(
+        self,
+        image_column_name: str,
+        latitude_column_name: str,
+        longitude_column_name: str,
+    ):
+        self.image_column_name = image_column_name
+        self.latitude_column_name = latitude_column_name
+        self.longitude_column_name = longitude_column_name
+
+    def __getitem__(self, idx):
+        img_path = self.images[idx]
+        gps = self.coordinates[idx]
+
+        img = image.open(os.path.join(self.dataset_folder, img_path)).convert('RGB')
+        
+        if self.transform:
+            img = self.transform(img)
+
+        return img, gps
---
+++ model/embedding_heads/alpha_clip_image.py
@@ -0,0 +1,40 @@
+import warnings
+
+import torch
+import torch.nn as nn
+from transformers import AutoProcessor, CLIPModel
+import alpha_clip
+
+from .base_encoder import BaseEncoder
+
+warnings.filterwarnings("ignore", category=UserWarning, module="huggingface_hub.*")
+
+
+class CLIPImageEncoder(BaseEncoder):
+    def __init__(self):
+        super(CLIPImageEncoder, self).__init__()
+        self.CLIP = alpha_clip.load("CLIP-L/14", alpha_vision_ckpt_pth='./weights/alpha_clip.pth')
+        self.image_processor = AutoProcessor.from_pretrained(
+            "openai/clip-vit-large-patch14"
+        )
+        self.mlp = nn.Sequential(nn.Linear(768, 768), nn.ReLU(), nn.Linear(768, 512))
+
+        # Freeze CLIP
+        for param in self.CLIP.parameters():
+            param.requires_grad = False
+
+    def load_weights(self, mlp_weights_path: str):
+        self.mlp.load_state_dict(torch.load(mlp_weights_path, weights_only=True))
+
+    def to(self, device: str):
+        self.CLIP.to(device)
+        self.mlp.to(device)
+
+    def preprocess(self, image: torch.Tensor) -> torch.Tensor:
+        x = self.image_processor(images=image, return_tensors="pt")["pixel_values"]
+        return x
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.CLIP.encode_image(pixel_values=x)
+        x = self.mlp(x)
+        return x
